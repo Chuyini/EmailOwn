83d5b2a38ed5918156e80de405568b6de5ff017e
Chuyini <jmlr231201@gmail.com>
2025-03-28 17:33:02 -0600

    Increasse de size of file upload

--- src/server.js
+++ src/server.js
@@ -6,15 +6,17 @@ const { generatePdfReport } = require('./pdfCreate');
 
 const app = express();
 
-// Middleware para CORS
+// Configurar CORS
 app.use(cors({
-  origin: 'https://formulario-pd-net.vercel.app', // Dominio de tu frontend
-  methods: ['GET', 'POST'],
+  origin: 'https://formulario-pd-net.vercel.app',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// Middleware para JSON
-app.use(express.json({ limit: '70mb' })); // Aumenta el límite de tamaño del cuerpo para manejar archivos grandes
+// Manejar preflight (OPTIONS)
+app.options('/send-email', cors());
 
+app.use(express.json({ limit: '70mb' }));
 // Endpoint para enviar correos
 app.post('/send-email', async (req, res) => {
   // No necesitas el uso de `res.header` aquí, ya que CORS lo maneja automáticamente con el middleware
