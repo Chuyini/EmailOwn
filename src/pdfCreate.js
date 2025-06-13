1fc200726a7857d9066777677108514d497ee67e
chuyini <jmlr231201@gmail.com>
2025-06-13 16:37:57 -0600

    add new endpoint domic

--- src/pdfCreate.js
+++ src/pdfCreate.js
@@ -201,7 +201,7 @@ async function generatePdfReportDomic(variables) {
     return new Promise((resolve, reject) => {
         const data = variables[0]; // Extraemos el objeto principal
 
-        const doc = new jsPDF();
+        const doc = PDFDocument({ size: "LETTER", margin: 50 });
 
         let y = 10; // Posición vertical inicial
         const addLine = (text, space = 10) => {
